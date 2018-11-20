ccfd00e0aa115afe93e8f8d1873e1ab80392e1b7
apoleshchuk <em167bm09021986Bm>
2018-11-20 15:38:06 +0300

    '...'

--- src/articles/article.js
+++ src/articles/article.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Article = ({article, handleClick}) => (
     <div className="article-item" onClick={() => handleClick(article)} >
-        <img className="article-image" src={article.urlToImage} alt={article.source.name} />
+        {article.urlToImage && (
+            <img className="article-image" src={article.urlToImage} alt={article.source.name} />
+        )}
         <h2 className="article-title">{article.title}</h2>
         <p className="article-description">{article.description}</p>
     </div>
--- src/articles/selected-article.js
+++ src/articles/selected-article.js
@@ -5,7 +5,9 @@ const SelectedArticle = ({article}) => (
         <h3 className="article-title">{article.title}</h3>
         <p><small>Source: <a href={article.url}>{article.source.name}</a> Date: {article.publishedAt} </small></p>
         <p>{article.content}</p>
-        <img src={article.urlToImage} alt={article.source.name} />              
+        {article.urlToImage && (
+            <img src={article.urlToImage} alt={article.source.name} />
+        )}
     </div>
 )
 
