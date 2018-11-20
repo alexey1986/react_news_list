6ac7175d6e3cdac75bda98b9d8e354c882bad851
apoleshchuk <em167bm09021986Bm>
2018-11-20 14:05:22 +0300

    Updates

---
+++ precache-manifest.a617a3e336a20c07542adf6c7794c2eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9241d06aa16442704cf0",
+    "url": "/react_news/static/js/runtime~main.9241d06a.js"
+  },
+  {
+    "revision": "6d7edbfb347ea37a8281",
+    "url": "/react_news/static/js/main.6d7edbfb.chunk.js"
+  },
+  {
+    "revision": "b50d3856f29df13ca0d9",
+    "url": "/react_news/static/js/1.b50d3856.chunk.js"
+  },
+  {
+    "revision": "6d7edbfb347ea37a8281",
+    "url": "/react_news/static/css/main.8084b2f1.chunk.css"
+  },
+  {
+    "revision": "44ae098cc4dc11cec4908f6a5850b302",
+    "url": "/react_news/index.html"
+  }
+];
